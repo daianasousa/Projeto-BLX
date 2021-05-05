6a28af5f6f2138f8829154e2407fd9685c03028e
daianasousa <daianasousa*04121996@gmail.com>
2021-05-04 23:35:28 -0300

    commit

---
+++ src/schemas/schemas.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class ProdutoSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    preco: float
+    disponivel: bool
+
+    class Config:
+        orm_mode = True
+
+
+class Usuario(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    telefone: str
+    senha: str
+    produtos: List[ProdutoSimples] = []
+
+    class Config:
+        orm_mode = True
+
+
+class UsuarioSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    telefone: str
+
+    class Config:
+        orm_mode = True
+
+
+class Produto(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+    usuario_id: Optional[int]
+    usuario: Optional[UsuarioSimples]
+
+    class Config:
+        orm_mode = True
+
+
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    quantidade: int
+    entrega: bool = True
+    endereco: str
+    observacoes: Optional[str] = 'Sem observações'
