a1cfb54cce02370ab5f2617b6023b7aec61125da
daianasousa <daianasousa*04121996@gmail.com>
2021-05-16 23:12:35 -0300

    Nova rota criada

--- src/routers/rotas_produtos.py
+++ src/routers/rotas_produtos.py
@@ -1,32 +1,38 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
-from src.schemas.schemas import Produto, ProdutoSimples
+from src.schemas.schemas import Produto, Produtosimples
 from src.infra.sqlalchemy.config.database import get_db
-from src.infra.sqlalchemy.repositorios.repositorio_produto import RepositorioProduto
+from src.infra.sqlalchemy.repositorios.repositorio_produto \
+    import RepositorioProduto
 
 router = APIRouter()
 
+@router.post('/produtos',
+             status_code=status.HTTP_201_CREATED,
+            response_model=Produtosimples)
 
-
-@router.post('/produtos', status_code=status.HTTP_201_CREATED, response_model=ProdutoSimples)
 def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
-
 
 @router.get('/produtos', response_model=List[Produto])
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
 
+@router.get('/produtos/{id}')
+def exibir_produto(id: int, session: Session = Depends(get_db)):
+    produto_localizado = RepositorioProduto(session).buscarPorId(id)
+    if not produto_localizado:
+        raise HTTPException(status_code=404, detail='Não Localizado produto')
+    return produto_localizado
 
-@router.put('/produtos/{id}', response_model=ProdutoSimples)
+@router.put('/produtos/{id}', response_model=Produtosimples)
 def atualizar_produto(id: int, produto: Produto, session: Session = Depends(get_db)):
     RepositorioProduto(session).editar(id, produto)
     produto.id = id
     return produto
-
 
 @router.delete('/produtos/{id}')
 def remover_produto(id: int, session: Session = Depends(get_db)):
