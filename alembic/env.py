48841e81a8c45eab64d4ef6f9bb891714f38514f
daianasousa <69603630+daianasousa@users.noreply.github.com>
2021-07-02 19:03:26 -0300

    Novas Atualizações.

--- alembic/env.py
+++ alembic/env.py
@@ -57,12 +57,12 @@ def run_migrations_online():
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+        poolclass=pool.NullPool,    
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
+            connection=connection, 
             target_metadata=target_metadata,
             render_as_batch=True
         )
